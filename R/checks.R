f05a3e517b95b5d6217a8deae203486c39cd235d
catalamarti <marticatalasabate@gmail.com>
2023-07-22 11:07:54 +0100

    change name of mockVocabulary

--- R/checks.R
+++ R/checks.R
@@ -13,3 +13,35 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
+
+#' Check if it is a list of tables from the same source
+#'
+#' @param listTables List of tables to check.
+#'
+#' @export
+#'
+checkListTables <- function(listTables) {
+
+}
+# checkInput(
+#   cdmVocabuly = cdmVocabuly, cdmName = cdmName, individuals = individuals,
+#   person = person, observation_period = observation_period, death = death,
+#   condition_occurrence = condition_occurrence, drug_exposure = drug_exposure,
+#   procedure_occurrence = procedure_occurrence,
+#   device_exposure = device_exposure, measurement = measurement,
+#   observation = observation, seed = seed, cdmTables = list(...)
+# )
+# checkInput(cdmTables = cdmTables, cdmName = cdmName)
+#
+# checkInput(
+#   cdm_source = cdm_source, concept = concept, vocabulary = vocabulary,
+#   domain = domain, concept_class = concept_class,
+#   concept_class= concept_class, concept_relationship = concept_relationship,
+#   concept_synonym = concept_synonym, concept_ancestor = concept_ancestor,
+#   source_to_concept_map = source_to_concept_map,
+#   drug_strength = drug_strength, cdm_version = cdm_version
+# )
+#
+# checkInput(
+#   intermediateAsTemp = intermediateAsTemp, cohortAsTemp = cohortAsTemp
+# )
