c851e010fca39ad780488ec81e58f183d8168ced
catalamarti <marticatalasabate@gmail.com>
2023-11-22 00:28:56 +0000

    play with compute

--- R/classGeneratedCohortSet.R
+++ R/classGeneratedCohortSet.R
@@ -337,14 +337,25 @@ defaultCohortAttrition <- function(cohort, set = NULL) {
     )
 }
 
-#' To add a new class that will go to the begining.
+#' To add a new class that will go to the beginning.
 #'
 #' @export
 #'
-#' @return Same object with the new clas added.
+#' @return Same object with the new class added.
 #'
 addClass <- function(x, class) {
+  x <- removeClass(x, class)
+  base::class(x) <- c(class, base::class(x))
+  return(x)
+}
+
+#' Remove class.
+#'
+#' @export
+#'
+#' @return Same object without the removed class.
+#'
+removeClass <- function(x, class) {
   base::class(x) <- base::class(x)[!(base::class(x) %in% class)]
-  base::class(x) <- c(class, base::clas(x))
   return(x)
 }
--- R/compute.R
+++ R/compute.R
@@ -21,14 +21,21 @@ cdmTable.cdm_reference <- function(cdm, x, name) {
   addClass(x, "cdm_table")
 }
 cdmTable.cdm_reference_from_con <- function(cdm, x, name) {
-  if (!"tbl_df" %in% class(x)) {
-    cli::cli_abort("x must be a tbl_df table.")
+  if ("tbl_df" %in% class(x)) {
+    fullName <- CDMConnector::inSchema(schema = attr(cdm, "write_schema"), table = name)
+    con <- attr(cdm, "dbcon")
+    DBI::dbWriteTable(conn = con, name = fullName, value = x, overwrite = TRUE)
+    x <- dplyr::tbl(src = con, fullName)
+    attr(x, "tbl_name") <- name
+  } else {
+    if (dplyr::same_src) {
+      cli::cli_abort("The table that you assign to a cdm object must be a local tbl or share the same source than the cdm")
+    }
+    if (!is.na(attr(x, "tbl_name")) && attr(x, "tbl_name") != name) {
+      cli::cli_abort("The table that you assign to a tbl object must be a temp table or a permanent table with the same name")
+      # do we want to automatically copy it?
+    }
   }
-  fullName <- CDMConnector::inSchema(schema = attr(cdm, "write_schema"), table = name)
-  con <- attr(cdm, "dbcon")
-  DBI::dbWriteTable(conn = con, name = fullName, value = x)
-  x <- dplyr::tbl(src = con, fullName)
-  attr(x, "tbl_name") <- name
   x <- addClass(x, c("cdm_table_from_con", "cdm_table"))
   return(x)
 }
@@ -42,7 +49,7 @@ cdmTable.cdm_reference_from_con <- function(cdm, x, name) {
 #' @return The cdm_table computed
 #'
 #' @importFrom dplyr compute
-compute.cdm_table <- function(x, name = attr(x, "tbl_name"), ...) {
+compute.cdm_table <- function(x, name = NULL, ...) {
   attr(x, "tbl_name") <- name
   return(x)
 }
@@ -50,27 +57,35 @@ compute.cdm_table <- function(x, name = attr(x, "tbl_name"), ...) {
 #' Compute a cdm_table_from_con
 #'
 #' @param x Table to compute.
-#' @param name Name of the table.
-#' @param temporary Whether to create a temporary of permanent table.
+#' @param name Name of the table. If Name is NULL a temporary table is computed.
+#' @param ... For compatibility.
 #'
 #' @return The cdm_table computed
 #'
 #' @importFrom dplyr compute
 compute.cdm_table_from_con <- function(x,
-                                       name = attr(x, "tbl_name"),
-                                       temporary = TRUE) {
+                                       name = NULL,
+                                       ...) {
   cdm <- attr(x, "cdm_reference")
-  if (temporary) {
+  if (is.null(name)) {
     name <- CDMConnector::uniqueTableName()
+    temporary <- TRUE
+    tblName <- NA_character_
+  } else {
+    temporary <- FALSE
+    tblName <- name
   }
+  x <- removeClass(x, c("cdm_table_from_con", "cdm_table"))
   x <- x %>%
     CDMConnector::computeQuery(
       name = name,
       temporary = temporary,
       schema = attr(cdm, "write_schema"),
-      overwrite = TRUE
+      overwrite = TRUE,
+      ...
     )
-  attr(x, "tbl_name") <- name
+  x <- addClass(x, c("cdm_table_from_con", "cdm_table"))
+  attr(x, "tbl_name") <- tblName
   return(x)
 }
 
