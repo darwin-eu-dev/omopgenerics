5444e496c8b7b44bff8b563474d119bc3e681a9c
catalamarti <marticatalasabate@gmail.com>
2023-12-09 19:59:21 +0000

    start cdm summary

---
+++ R/methodSummary.R
@@ -0,0 +1,11 @@
+
+#' Summary a cdm reference
+#'
+#' @param object A cdm reference object.
+#' @param ... For compatibility (not used).
+#'
+#' @export
+#'
+summary.cdm_reference <- function(object, ...) {
+
+}
