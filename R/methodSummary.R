c79e65f3f83aa179a209c03eea32f033efdb3e17
catalamarti <marticatalasabate@gmail.com>
2023-12-09 20:00:18 +0000

    start cohort summary

--- R/methodSummary.R
+++ R/methodSummary.R
@@ -9,3 +9,14 @@
 summary.cdm_reference <- function(object, ...) {
 
 }
+
+#' Summary a generated cohort set
+#'
+#' @param object A generated cohort set object.
+#' @param ... For compatibility (not used).
+#'
+#' @export
+#'
+summary.generated_cohort_set <- function(object, ...) {
+
+}
