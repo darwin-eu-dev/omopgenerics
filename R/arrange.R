fc3401750a5ec54cdeba9fd278c1c1ce5131f81a
catalamarti <marticatalasabate@gmail.com>
2023-12-11 01:11:47 +0000

    Update arrange.R

--- R/arrange.R
+++ R/arrange.R
@@ -18,7 +18,9 @@
 #' @importFrom dplyr arrange
 arrange.cdm_table <- function(.data, ...) {
   if ("tbl_lazy" %in% class(.data)) {
+    removeClass(.data) <- "cdm_table"
     x <- dbplyr::window_order(.data, ...)
+    addClass(.data) <- "cdm_table"
   } else {
     removeClass(.data) <- "cdm_table"
     x <- dplyr::arrange(.data, ...)
