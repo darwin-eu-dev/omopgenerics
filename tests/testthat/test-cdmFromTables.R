61849bc0c87da392f89dc134b693cf7445671c8c
catalamarti <marticatalasabate@gmail.com>
2024-01-04 22:53:13 +0000

    failing test

--- tests/testthat/test-cdmFromTables.R
+++ tests/testthat/test-cdmFromTables.R
@@ -57,4 +57,23 @@ test_that("test cdmFromTables", {
   )
   expect_identical(cdmName(cdm), "mocktest")
 
+  cdmFromTables(
+    tables = list(
+      "person" = dplyr::tibble(
+        person_id = 1, gender_concept_id = 0, year_of_birth = 1990,
+        race_concept_id = 0, ethnicity_concept_id = 0
+      ),
+      "observation_period" = dplyr::tibble(
+        observation_period_id = 1, person_id = 1,
+        observation_period_start_date = as.Date("2000-01-01"),
+        observation_period_end_date = as.Date("2025-12-31"),
+        period_type_concept_id = 0
+      ),
+      "cdm_source" = dplyr::tibble(
+        cdm_source_name = "test", cdm_version = NA
+      )
+    ),
+    cdmName = "mock"
+  )
+
 })
