0efd7a509b9153068fe5961843e33d3c8c4a415c
catalamarti <marticatalasabate@gmail.com>
2024-04-08 19:41:38 +0100

    Update classSummarisedResult.R

--- R/classSummarisedResult.R
+++ R/classSummarisedResult.R
@@ -450,9 +450,10 @@ addSettings <- function(result, columns = NULL) {
   if (length(notPresent)) {
     cli::cli_abort("The following columns are not present in settings: {paste0(notPresent, collapse = ', ')}.")
   }
+  toJoin <- columns[columns %in% colnames(result)]
   result |>
     dplyr::left_join(
       set |> dplyr::select(dplyr::all_of(c("result_id", columns))),
-      by = "result_id"
+      by = c("result_id", toJoin)
     )
 }
