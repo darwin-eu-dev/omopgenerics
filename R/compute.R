f1bb5b3c830bc70d0efb1a99beb5b59d2a476f67
catalamarti <marticatalasabate@gmail.com>
2024-04-18 15:47:20 +0100

    Update compute.R
    fix issue #254

--- R/compute.R
+++ R/compute.R
@@ -66,7 +66,7 @@ compute.local_cdm <- function(x, ...) {
 #' uniqueTableName()
 uniqueTableName <- function(prefix = "") {
   assertCharacter(x = prefix, length = 1)
-  i <- getOption("dbplyr_table_name", 0) + 1
+  i <- getOption("og_table_name", 0) + 1
   options(dbplyr_table_name = i)
   value <- paste0(sprintf("og_%03i", i), "_", round(as.numeric(Sys.time())))
   paste0(prefix, value)
