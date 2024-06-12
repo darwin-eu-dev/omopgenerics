9a7b682763b5c80cbf953342d922d6ef24f3eb5b
catalamarti <marticatalasabate@gmail.com>
2024-06-12 15:39:54 +0100

    one main function

--- R/validate.R
+++ R/validate.R
@@ -14,6 +14,72 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+#' Input validation of arguments, arguments will be corrected accordingly.
+#' You can check the possible validated arguments and their validation in
+#' `argumentsValidation()`.
+#'
+#' @param ... Named arguments to validate.
+#' @param call Call object passed to cli messages.
+#' @param .options named list of internal arguments to be passed to the
+#' validation functions
+#'
+#' @export
+#'
+inputValidation <- function(..., call = parent.frame(), .options = list()) {
+
+}
+
+#' Details on the performed validation per argument.
+#'
+#' @param argumentName The of the argument to see the details. If NULL all
+#' available arguments are displayed.
+#' @param verbose Whether to print the details
+#'
+#' @return A tibble with the details of the validation (invisible).
+#'
+#' @export
+#'
+#' @examples
+#' argumentsValidation("cohortId")
+#' argumentsValidation()
+#'
+argumentsValidation <- function(argumentName = NULL, verbose = TRUE) {
+  # input validation
+  assertCharacter(argumentName, null = TRUE)
+
+  if (is.null(argumentName)) {
+    x <- argumentValidation
+  } else {
+    x <- argumentValidation |>
+      dplyr::filter(.data$argument_name %in% .env$argumentName)
+  }
+
+  if (nrow(x) == 0) return(invisible(x))
+
+  xx <- "\u00a0\u00a0\u00a0\u00a0-" |> glue::glue()
+  cli::cli_h1("Input validation")
+  for (k in seq_len(nrow(x))) {
+    cli::cli_h3("Argument: {.field {x$argument_name[[k]]}}")
+    cli::cli_inform(c("*" = "{.string Validation}: {x$validation[[k]]}"))
+    for (i in seq_along(x$required_arguments[[k]])) {
+      if (i == 1) cli::cli_inform(c("*" = "{.string Required arguments}:"))
+      nm <- names(x$required_arguments[[k]])
+      mes <- x$required_arguments[[k]][[i]]
+      cli::cli_text("{xx} {.var {nm}}: {mes}.")
+    }
+    for (i in seq_along(x$optional_arguments[[k]])) {
+      if (i == 1) cli::cli_inform(c("*" = "{.string Optional arguments}:"))
+      nm <- names(x$optional_arguments[[k]])
+      mes <- x$optional_arguments[[k]][[i]]
+      cli::cli_text("{xx} {.var {nm}}: {mes}.")
+    }
+  }
+  cli::cli_text()
+
+  return(invisible(x))
+}
+
+
 #' Validate that the a variable is a valid name for a table in the cdm. A
 #' message will be shown if name had to be modified and/or the name is already
 #' part of the cdm.
