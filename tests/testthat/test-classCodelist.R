1e4a66c638781c03806bd8b4c9dc3b8ab203a0db
edward-burn <9583964+edward-burn@users.noreply.github.com>
2024-07-02 17:47:35 +0100

    separate class

--- tests/testthat/test-classCodelist.R
+++ tests/testthat/test-classCodelist.R
@@ -15,9 +15,18 @@ test_that("test codelist works", {
   expect_true(c("codelist") %in% class)
   expect_no_error(print(codes))
 
+
+  expect_no_error(emptyCodelist())
+
   # expected errors
   expect_error(newCodelist(c(1,2,3)))
   expect_error(newCodelist(list(c(1,2,3))))
+
+  codes <- list("disease" = dplyr::tibble(concept_id = c(1,2,3),
+                                          other_info = c("a", "b", "c")))
+
+
+  expect_error(codes <- newCodelist(codes))
 
   codes <- list("disease X" = c(1,NA,3),
                 "disease Y" = c(4,5))
@@ -30,43 +39,4 @@ test_that("test codelist works", {
 
 })
 
-test_that("test codelist with details", {
-
-  # single codelist
-  codes <- list("disease" = dplyr::tibble(concept_id = c(1,2,3),
-                                          other_info = c("a", "b", "c")))
-  expect_no_error(codes <- newCodelist(codes))
-  class <- codes |> newCodelist() |> class()
-  expect_true(c("codelist") %in% class)
-  expect_no_error(print(codes))
-
-
-  # multiple codelists
-  codes <- list("disease X" = dplyr::tibble(concept_id = c(1,2,3),
-                                            other_info = c("a", "b", "c")),
-                "disease Y" = dplyr::tibble(concept_id = c(4,5),
-                                            other_info = c("d", "e")))
-  expect_no_error(codes <- newCodelist(codes))
-  class <- codes |> newCodelist() |> class()
-  expect_true(c("codelist") %in% class)
-  expect_no_error(print(codes))
-
-
-  # expected errors
-  expect_error(newCodelist(list(dplyr::tibble(concept_id = c(1,2,3)))))
-  # cannot be mixed
-  codes <- list("disease X" = dplyr::tibble(concept_id = c(1,2,3),
-                                            other_info = c("a", "b", "c")),
-                "disease Y" = c(4,5))
-  expect_error(codes <- newCodelist(codes))
-
-
-  codes <- list("disease X" = dplyr::tibble(concept_id = c(1,NA,3),
-                                            other_info = c("a", "b", "c")),
-                "disease Y" = dplyr::tibble(concept_id = c(4,5),
-                                            other_info = c("d", "e")))
-  expect_error(codes <- newCodelist(codes))
-
-
-})
 
---
+++ tests/testthat/test-classCodelistWithDetails.R
@@ -0,0 +1,43 @@
+test_that("test codelist with details", {
+
+  # single codelist
+  codes <- list("disease" = dplyr::tibble(concept_id = c(1,2,3),
+                                          other_info = c("a", "b", "c")))
+
+  expect_no_error(codes <- newCodelistWithDetails(codes))
+  expect_true(inherits(codes, "codelist_with_details"))
+  expect_no_error(print(codes))
+
+
+  # multiple codelists
+  codes <- list("disease X" = dplyr::tibble(concept_id = c(1,2,3),
+                                            other_info = c("a", "b", "c")),
+                "disease Y" = dplyr::tibble(concept_id = c(4,5),
+                                            other_info = c("d", "e")))
+  expect_no_error(codes <- newCodelistWithDetails(codes))
+  expect_true(inherits(codes, "codelist_with_details"))
+  expect_no_error(print(codes))
+
+  expect_no_error(emptyCodelistWithDetails())
+
+  # expected errors
+  codes <- list("disease" = c(1,2,3))
+  expect_error(codes <- newCodelistWithDetails(codes))
+
+  codes <- list("disease" = dplyr::tibble(other_info = c("a", "b", "c")))
+  expect_error(codes <- newCodelistWithDetails(codes))
+
+  expect_error(newCodelistWithDetails(list(dplyr::tibble(concept_id = c(1,2,3)))))
+  # cannot be mixed
+  codes <- list("disease X" = dplyr::tibble(concept_id = c(1,2,3),
+                                            other_info = c("a", "b", "c")),
+                "disease Y" = c(4,5))
+  expect_error(codes <- newCodelistWithDetails(codes))
+
+
+  codes <- list("disease" = c(1,2,3))
+  expect_error(codes <- newCodelistWithDetails(codes))
+
+  expect_no_error(emptyCodelistWithDetails()
+
+})
